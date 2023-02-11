175e1d2f75b232f981ef2de98b5b2806baa17aee
Brad <117415722+bktpp@users.noreply.github.com>
2023-02-11 01:36:26 -0600

    started styling. built calculator shell + grid

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const equals = document.querySelector('.equals');
 
 let numbersToAdd = [];
 let holdingArea = '';
-let plusSwitch = false;
-let minusSwitch = false;
 let clear = false;
 
 console.log(numbersToAdd);
@@ -34,26 +32,34 @@ numbers.forEach(function(number) {
             holdingArea += '9';
         } else if (number.classList.contains('zero')) {
             holdingArea += '0';
-        } 
+        } else if (number.classList.contains('decimal')) {
+            holdingArea += '.';
+        
         // operators
-          else if (number.classList.contains('plus')) {
-            plusSwitch = true;
+        }  else if (number.classList.contains('plus')) {
             parsingFunc();
             numbersToAdd.push('+');
-            console.log(numbersToAdd);
-        } 
-        
-        else if(number.classList.contains('minus')) {
-            minusSwitch = true;
+        }  else if (number.classList.contains('minus')) {
             parsingFunc();
             numbersToAdd.push('-');
+        }  else if (number.classList.contains('divide')) {
+            parsingFunc();
+            numbersToAdd.push('/');
+        }  else if (number.classList.contains('multiply')) {
+            parsingFunc();
+            numbersToAdd.push('*');
+            console.log(numbersToAdd);
+        }  else if (number.classList.contains('clear')) {
+            numbersToAdd = [];
+            holdingArea = '';
+            added = '';
             console.log(numbersToAdd);
         }
       })
     });
 
 function parsingFunc() {
-    let added = parseInt(holdingArea);
+    let added = parseFloat(holdingArea);
     numbersToAdd.push(added);
     holdingArea = '';
     added = '';
@@ -61,12 +67,13 @@ function parsingFunc() {
 
 //master function for adding numbers selected by user returning answer
 equals.addEventListener('click', function() {
-    let added = parseInt(holdingArea);
+    let added = parseFloat(holdingArea);
     numbersToAdd.push(added);
     holdingArea = '';
     added = '';
     calculate(numbersToAdd);
     console.log(calculate(numbersToAdd));
+    console.log(numbersToAdd);
 })
 
 function calculate(equation) {
@@ -74,75 +81,52 @@ function calculate(equation) {
     let operatorIndex = 1;
     if (typeof equation[0] === "number") {
       result = equation[0];
-}   else if (isNaN(equation[0])) {
-      console.log(NaN + " first iteration"); //will make pop up saying invalid format
-}   else {
-      return NaN; //will make pop up saying invalid format
-}   for (let i = operatorIndex; i < equation.length; i += 2) {
-        if (typeof equation[i] !== "string") {
-            return NaN // possible pop up
-        }
-          switch (equation[i]) {
-            case "+":
-               if (typeof equation[i + 1] === "number") {
-                   result += equation[i + 1];
-               } else {
-                   return NaN;
-               }
-               break;
-
-           case "-":
-               if (typeof equation[i + 1] === "number") {
-                   result -= equation[i + 1];
-               } else {
-                   return NaN;
-               }  
-               break;
-               default:
-                return NaN;
-}         
-               
-}
-            return result;
-}
-
-//after numbers are parsed and put in array, this function will add the integers
-function addNum(...args) {
-    let sum = 0;
-    for (let i = 0; i < args.length; i++) {
-        sum += args[i];
-        result = sum;
+    } else if (isNaN(equation[0])) {
+      console.log(NaN + " first iteration");
+    } else {
+      return NaN;
+    }
+    for (let i = operatorIndex; i < equation.length; i += 2) {
+      if (typeof equation[i] !== "string") {
+        return NaN;
+      }
+      switch (equation[i]) {
+        case "+":
+          if (typeof equation[i + 1] === "number") {
+            result += equation[i + 1];
+          } else {
+            return NaN;
+          }
+          break;
+        case "-":
+          if (typeof equation[i + 1] === "number") {
+            result -= equation[i + 1];
+          } else {
+            return NaN;
+          }  
+          break;
+        case "*":
+          if (typeof equation[i + 1] === "number") {
+            result *= equation[i + 1];
+            result = result.toFixed(2);
+          } else {
+            return NaN;
+          }  
+          break;
+        case "/":
+          if (typeof equation[i + 1] === "number") {
+            result /= equation[i + 1];
+          } else {
+            return NaN;
+          }  
+            break;  
+        default:
+          return NaN;
+      }
     }
     return result;
-}
-
-// after numbers are parsed and put in array, this function will subtract the integers
-function minusNum(...subtract) {
-    let sum = numbersToAdd[0];
-    for (let i = 1; i < subtract.length; i++) {
-        sum -= subtract[i];
-    }
-    return sum;
-}
-
-
-/* document.addEventListener('keydown', function(event) {
-    if (event.code === 'Key1') {
-        holdingArea += '1';
-    } else if (event.code === 'Key2') {
-        holdingArea += '2';
-    } else if (event.code === 'Key3') {
-        holdingArea += '3';
-    } else if (event.code === 'NumpadAdd') {
-        plusSwitch = true;
-        parsingFunc();
-    } else if (event.code === 'NumpadSubtract') {
-        minusSwitch = true;
-        parsingFunc();
-    }
-}); */ //need to revisit this for numberpad intergration  
-
-
+  }
+  
 
 
 
